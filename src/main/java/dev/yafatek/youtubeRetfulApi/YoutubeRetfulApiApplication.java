6df0013cf4d350e2d636852734f4ac154458a2b9
Feras <feras.alawadi@zarcony.com>
2020-06-26 21:09:04 +0300

    init repo

---
+++ src/main/java/dev/yafatek/youtubeRetfulApi/YoutubeRetfulApiApplication.java
@@ -0,0 +1,13 @@
+package dev.yafatek.youtubeRetfulApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeRetfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeRetfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yafatek/youtubeRetfulApi/YoutubeRetfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yafatek.youtubeRetfulApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeRetfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
